b08a6ff47721122432fd81f6687f781e46ab0281
Jerrin Joe Varghese <jvargh81@lakers.mercyhurst.edu>
2018-11-29 00:41:58 -0500

    Updating exit and remove function.

--- midterm.js
+++ midterm.js
@@ -2,7 +2,6 @@ async function main(){
   var w=500;
   var h=500;
   var padding=60;  
-  var tracker=20;  
   var yValue=440;
 
   var svg = d3.select("body")
@@ -10,7 +9,7 @@ async function main(){
               .attr("width",w)
               .attr("height",h)
  
-  var salary = await d3.csv("salaries.csv")
+  var salary = await d3.csv("salaries.csv");
 
   /* Parsing integer strings to integer. */
   salary = salary.map(d=>({
@@ -20,96 +19,90 @@ async function main(){
           }));
               
   /* Preparing the opening page. */   
-  var dataset = salary.filter(d=>d["name"]=="Anaheim Angels")
+  var dataset = salary.filter(d=>d["name"]=="Cleveland Blues");
 
-            svg.append("text")
-              .attr("x",180)
-              .attr("y",15)
-              .text("MLB Payrolls Over the Years")
-              .attr("font-size",20)
-              .attr("text-anchor","middle")
+  svg.append("text")
+    .attr("x",180)
+    .attr("y",15)
+    .text("MLB Payrolls Over the Years")
+    .attr("font-size",20)
+    .attr("text-anchor","middle")
 
-            svg.append("text")
-              .attr("x",180)
-              .attr("y",35)
-              .text("Select the team, then click on the year to see the payroll")
-              .attr("font-size",12)
-              .attr("text-anchor","middle")
+  svg.append("text")
+    .attr("x",180)
+    .attr("y",35)
+    .text("Select the team, then click on the year to see the payroll")
+    .attr("font-size",12)
+    .attr("text-anchor","middle")
 
-            var ymin = 0;
-            var ymax = 240;
-            var xmin = 0;
-            var xmax = 480;
+  var ymin = 0;
+  var ymax = 240;
+  var xmin = 0;
+  var xmax = 480;
 
-            var xScale = d3.scaleLinear()
-                           .domain([xmin,xmax])
-                           .range([15+padding,w-padding]);
+  var xScale = d3.scaleLinear()
+                  .domain([xmin,xmax])
+                  .range([15+padding,w-padding]);
+  
+  var yScale = d3.scaleLinear()
+                  .domain([ymax,ymin])
+                  .range([0+padding,h-padding]);
+          
+  var yAxis = d3.axisLeft()
+                .scale(yScale);
+  
+  svg.append("g")
+      .attr("class","yaxis")
+      .attr("transform","translate("+padding+",0)")
+      .call(yAxis);
+  
+  svg.append("text")
+      .data(dataset)
+      .attr("class","title")
+      .attr("x",300)
+      .attr("y",100)
+      .text(function(d){return d["name"]})
+      .attr("font-size",12)
+
+ circle =  svg.append("circle")
+      .attr("class","circles")
+      .attr("cx",60)
+      .attr("cy",yValue)
+      .attr("r",5)
+      .attr("fill","red")
+      .attr("stroke","black")
             
-            var yScale = d3.scaleLinear()
-                           .domain([ymax,ymin])
-                           .range([0+padding,h-padding]);
-                    
-            var yAxis = d3.axisLeft()
-                          .scale(yScale);
-            
-            svg.append("g")
-               .attr("class","yaxis")
-               .attr("transform","translate("+padding+",0)")
-               .call(yAxis);
-            
-            svg.append("text")
-               .data(dataset)
-               .attr("class","title")
-               .attr("x",300)
-               .attr("y",100)
-               .text(function(d){return d["name"]})
-               .attr("font-size",12)
+  text = d3.select("svg")
+      .selectAll('text.years')
+      .data(dataset)
+      .enter()
+      .append("text")
+      .classed('years', true)	
+      .text(function(d){return d.year})
+      .attr("x", function(d,i){return xScale(100 * (i % 2))})
+      .attr("y",function(d,i){return yScale(10 * (Math.floor(i/2)))})
+      .attr("font-size",12)
+      .on("click", function(d){
+          d3.selectAll(".circles")
+            .attr("cy",yScale(d["salaries"]))
+            })
 
-            svg.append("circle")
-               .attr("class","circles")
-               .attr("cx",60)
-               .attr("cy",yValue)
-               .attr("r",5)
-               .attr("fill","red")
-               .attr("stroke","black")
-                     
-            d3.select("svg")
-               .selectAll('text.cityname')
-               .data(dataset)
-               .enter()
-               .append("svg:text")
-               .classed('cityname', true)	
-               .text(function(d){return d["year"]})
-               .attr("x", function(d,i){return xScale(100 * (i % 2))})
-               .attr("y",function(d,i){return yScale(10 * (Math.floor(i/2)))})
-               .attr("font-size",12)
-               .on("click", function(d){
-                    d3.selectAll(".circles")
-                      .attr("cy",yScale(d["salaries"]))
-                      })
-
-    
-            d3.selectAll("option")
-                /*Click Functionality */
-               .on("click", function(){ 
-                dataset = salary.filter(d=>d["name"]==this.value)
-                
-                console.log(dataset)
-
-                d3.select(".title")
-                  .text(this.value)
-
-                d3.selectAll(".cityname")
-                .data(dataset)	
-                .text(function(d){return d["year"]}) 
-                .attr("x", function(d,i){return xScale(100 * (i % 2))})
-                .attr("y",function(d,i){return yScale(10 * (Math.floor(i/2)))})
-                .on("click", function(d){
-                  d3.selectAll(".circles")
-                    .attr("cy",yScale(d["salaries"]))
-                    })                             
-               })
-               
+  d3.selectAll("option")
+      /*Click Functionality */
+      .on("click", function(){ 
+        dataset = salary.filter(d=>d["name"]==this.value);
+        console.log(dataset)
+        d3.select(".title")
+          .text(this.value)
+        
+        d3.selectAll(".years")
+            .data(dataset)
+            .attr("x", function(d,i){return xScale(100 * (i % 2))})
+            .attr("y",function(d,i){return yScale(10 * (Math.floor(i/2)))})
+            .text(function(d){return d["year"]}) 
+            .exit()
+            .remove()
+        })                
 }
 
 main();
